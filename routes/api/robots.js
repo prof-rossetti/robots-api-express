09ba0aa12883c5885a472965ed2c8693a9dad480
MJ Rossetti <datacreativellc@gmail.com>
2017-04-12 17:44:23 -0400

    Document process of running on a different port

--- routes/api/robots.js
+++ routes/api/robots.js
@@ -2,14 +2,14 @@ var express = require('express');
 var router = express.Router();
 var Robot = require("../../models/robot");
 
-/* List Robots (GET) */
+/* LIST */
 
 router.get('/api/robots.json', function(req, res, next) {
   Robot.find( function (err, robots) {
     if (err) {
       console.log(err)
       res.setHeader('Content-Type', 'application/json')
-      res.status(404).json("OOPS. SERVER ERROR.")
+      res.status(500).json("OOPS. SERVER ERROR.")
     } else {
       console.log("LIST", robots.length, "ROBOTS:", robots)
       res.setHeader('Content-Type', 'application/json')
@@ -18,7 +18,14 @@ router.get('/api/robots.json', function(req, res, next) {
   })
 })
 
-/* Show Robot (GET) */
+/* CREATE */
+
+router.post('/api/robots.json', function(req, res, next) {
+  res.setHeader('Content-Type', 'application/json')
+  res.status(200).json("TODO: CREATE")
+})
+
+/* SHOW */
 
 router.get('/api/robots/:id.json', function(req, res, next) {
   console.log(req.params)
@@ -26,7 +33,7 @@ router.get('/api/robots/:id.json', function(req, res, next) {
 
   Robot.findById(robotId, function(err, robot) {
     if (err){
-      errorMessage = `Couldn't show robot ${robotId}. Please double-check you are requesting a robot with a valid identifier. Try listing all robots to see some examples.`
+      errorMessage = `Couldn't find robot ${robotId}. Please double-check you are requesting a robot with a valid identifier. Try listing all robots to see some examples.`
       console.log(errorMessage)
       res.setHeader('Content-Type', 'application/json')
       res.status(404).json(errorMessage)
@@ -34,8 +41,22 @@ router.get('/api/robots/:id.json', function(req, res, next) {
       console.log("SHOW ROBOT", robot);
       res.setHeader('Content-Type', 'application/json')
       res.status(200).json(robot)
-    };
-  });
-});
+    }
+  })
+})
+
+/* UPDATE */
+
+router.patch('/api/robots/:id.json', function(req, res, next) {
+  res.setHeader('Content-Type', 'application/json')
+  res.status(200).json("TODO: UPDATE")
+})
+
+/* DESTROY */
+
+router.delete('/api/robots/:id.json', function(req, res, next) {
+  res.setHeader('Content-Type', 'application/json')
+  res.status(200).json("TODO: DESTROY")
+})
 
 module.exports = router;
